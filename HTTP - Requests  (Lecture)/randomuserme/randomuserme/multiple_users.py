ea52ac86ed2c7647117d81c9e5dfce9f39bb19ef
ZARIF ubuntu <uzbzarif@gmail.com>
2021-01-26 22:33:33 +0900

    multiple users request

---
+++ HTTP - Requests  (Lecture)/randomuserme/randomuserme/multiple_users.py
@@ -0,0 +1,16 @@
+
+import requests
+def multiple_users(number:int)->dict:
+    '''
+    
+    '''
+    # using f-string make request URL for users
+    url = ''
+    r = requests.get(url)
+
+    return {}
+# The requests module allows you to send HTTP requests using Python.
+
+
+
+ 
